0f9b9a66facacfcfc0966f277150c9ddbde02a1d
Preston Miller Firestone <firestone.preston@gmail.com>
2026-04-09 16:15:58 +0530

    Add kulupu.la DNS records.
    Not sure about the formatting or how to integrate this into the rest of the
    script. Also not sure about secret management. But at least everything's now
    formatted for DNSControl.

--- dnsconfig.js
+++ dnsconfig.js
@@ -191,3 +191,20 @@ D_EXTEND("toktok.pona.la", CNAME("@", "hosting.adalo.com."));
 // utala.pona.la
 // lawa li jan Lakuse @raacz
 D_EXTEND("utala.pona.la", CNAME("@", "utala-pona-la.netlify.app."));
+
+// kulupu.la
+D("kulupu.la", REG_PORKBUN, DnsProvider(DSP_PORKBUN),
+  DefaultTTL(600),
+  A("@", "46.23.81.157"), // lipu Sourcehut
+  AAAA("@", "2a03:6000:1813:1337::157"), // lipu Sourcehut
+  CNAME("www", "@"), // o pona e nasin WWW
+
+  // ilo Purelymail
+  MX("@", 0, "mailserver.purelymail.com."),
+  CNAME("purelymail1._domainkey.kulupu.la.", "key1.dkimroot.purelymail.com."),
+  CNAME("purelymail2._domainkey.kulupu.la.", "key2.dkimroot.purelymail.com."),
+  CNAME("purelymail3._domainkey.kulupu.la.", "key3.dkimroot.purelymail.com."),
+  CNAME("_dmarc.kulupu.la.", "dmarcroot.purelymail.com."),
+  TXT("@", "purelymail_ownership_proof=b459e5f97d39acafb4b7f8249cf21e98f46e16d445c7d86bb22af1a9f00a91a2a33467efab26eed13b34697eaafeafe770cba18618bf1b5b7284b5465a146b0a"),
+  TXT("@", "v=spf1 include:_spf.purelymail.com ~all")
+ )
